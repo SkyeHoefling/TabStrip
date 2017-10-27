575213d085a6d3b8d31a44cbe6e15bc30099c733
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-10-27 01:14:00 +0000

    Converted Abstractions project from PCL to net standard 2.0

--- src/TabStrip.FormsPlugin.Abstractions/HelloView.xaml.cs
+++ src/TabStrip.FormsPlugin.Abstractions/HelloView.xaml.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TabStrip.FormsPlugin.Abstractions
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HelloView : ContentView
-    {
-        public HelloView(bool init) { }
-        public HelloView()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class HelloView : ContentView
+	{
+		public HelloView ()
+		{
+			InitializeComponent ();
+		}
+	}
 }
