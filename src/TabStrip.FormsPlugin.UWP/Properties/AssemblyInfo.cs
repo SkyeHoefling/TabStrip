bbf5e6a928e6e27694586a6a9e16fafd34c20ede
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-11-04 02:05:35 +0000

    Updated namespaces to TabStrip.FormsPlugin.UWP project to match TabStrip since it was originally a clone on CarouselView. This was causing a build problem conflicting with the 2 UWP projects (CarouselView and TabStrip)

--- src/TabStrip.FormsPlugin.UWP/Properties/AssemblyInfo.cs
+++ src/TabStrip.FormsPlugin.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CarouselView.FormsPlugin.UWP")]
+[assembly: AssemblyTitle("TabStrip.FormsPlugin.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CarouselView.FormsPlugin.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("TabStrip.FormsPlugin.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
